f42a5e3d6d80c6bdcdb5d8feea3d9443aed230dd
Olzhas Aitkaliyev <olzhas.aitkaliyev@gmail.com>
2016-09-21 00:17:14 +0300

    Initial commit

---
+++ benchmark/src/Benchmark.java
@@ -0,0 +1,57 @@
+/**
+ * Created by olzhas on 20.09.2016.
+ */
+import com.google.caliper.BeforeExperiment;
+import com.google.caliper.Param;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Benchmark {
+    @Param
+    private String pathToData = "";
+
+    private double[] array;
+    private double[] arrayCopy;
+    private Sorter sorter;
+
+
+    @BeforeExperiment
+    public void setUp() throws IOException {
+        sorter = new Sorter();
+        loadData();
+        arrayCopy = new double[array.length];
+    }
+
+    @com.google.caliper.Benchmark
+    public void timeSortArray(int reps) {
+        for (int i = 0; i < reps; i++) {
+            System.arraycopy(array, 0, arrayCopy, 0, array.length);
+            sorter.sort(arrayCopy);
+        }
+    }
+
+    private void loadData() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(pathToData));
+
+        ArrayList<Double> doubles = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            double number;
+            try {
+                number = Double.parseDouble(line);
+            } catch (NumberFormatException ex){
+                continue;
+            }
+            doubles.add(number);
+        }
+
+        br.close();
+
+        array = new double[doubles.size()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = doubles.get(i);
+        }
+    }
+}
+
---
+++ benchmark/src/StartBenchmark.java
@@ -0,0 +1,10 @@
+/**
+ * Created by olzhas on 20.09.2016.
+ */
+import com.google.caliper.runner.CaliperMain;
+
+public class StartBenchmark {
+    public static void main(String[] args) {
+        CaliperMain.main(Benchmark.class, args);
+    }
+}
